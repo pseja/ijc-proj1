d970034f6a29556fc339a8821b39506086f71fdb
Lukas Pseja <luk4sps3j4@gmail.com>
2024-03-08 15:22:45 +0100

    Inline functions have to be defined in bitset.c file for it to work with -O0

--- bitset.c
+++ bitset.c
@@ -1,6 +1,54 @@
 #include "bitset.h"
 
-int main()
+#ifdef USE_INLINE
+
+inline void bitset_free(bitset_t jmeno_pole)
 {
-    return 0;
+    free(jmeno_pole);
 }
+
+inline bitset_index_t bitset_size(bitset_t jmeno_pole)
+{
+    return jmeno_pole[0];
+}
+
+inline void bitset_setbit(bitset_t jmeno_pole, bitset_index_t index, bool bool_vyraz)
+{
+    bitset_index_t index = index + ULONG_BITS;
+    bitset_index_t velikost = bitset_size(jmeno_pole);
+
+    if (index >= velikost)
+    {
+        error_exit("bitset_setbit: Index %lu mimo rozsah 0..%lu", index - ULONG_BITS, velikost);
+    }
+    else
+    {
+        bitset_index_t maska = 1UL << (index % ULONG_BITS);
+
+        if (bool_vyraz)
+        {
+            jmeno_pole[index / ULONG_BITS] |= maska;
+        }
+        else
+        {
+            jmeno_pole[index / ULONG_BITS] &= ~maska;
+        }
+    }
+}
+
+inline bitset_index_t bitset_getbit(bitset_t jmeno_pole, bitset_index_t index)
+{
+    bitset_index_t index = index + ULONG_BITS;
+    bitset_index_t velikost = bitset_size(jmeno_pole);
+
+    if (index >= velikost)
+    {
+        error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu", index - ULONG_BITS, velikost);
+    }
+    else
+    {
+        return (jmeno_pole[(index / ULONG_BITS) + 1] >> (index % ULONG_BITS)) & 1UL;
+    }
+}
+
+#endif /* USE_INLINE */
--- bitset.h
+++ bitset.h
@@ -102,55 +102,16 @@ TODO: vyřešit inline funkce
 #else
 
 // Frees the bitset
-inline void bitset_free(bitset_t jmeno_pole)
-{
-    free(jmeno_pole);
-}
+void bitset_free(bitset_t jmeno_pole);
 
 // Returns the size of the bitset
-inline bitset_index_t bitset_size(bitset_t jmeno_pole)
-{
-    return jmeno_pole[0];
-}
+bitset_index_t bitset_size(bitset_t jmeno_pole);
 
 // Sets the bit at the given index to the given boolean value
-inline void bitset_setbit(bitset_t jmeno_pole, bitset_index_t index, bool bool_vyraz)
-{
-    bitset_index_t index = index + ULONG_BITS;
-    bitset_index_t velikost = bitset_size(jmeno_pole);
-    if (index >= velikost)
-    {
-        error_exit("bitset_setbit: Index %lu mimo rozsah 0..%lu", index - ULONG_BITS, velikost);
-    }
-    else
-    {
-        bitset_index_t maska = 1UL << (index % ULONG_BITS);
-
-        if (bool_vyraz)
-        {
-            jmeno_pole[index / ULONG_BITS] |= maska;
-        }
-        else
-        {
-            jmeno_pole[index / ULONG_BITS] &= ~maska;
-        }
-    }
-}
+void bitset_setbit(bitset_t jmeno_pole, bitset_index_t index, bool bool_vyraz);
 
 // Returns the bit at the given index
-inline bitset_index_t bitset_getbit(bitset_t jmeno_pole, bitset_index_t index)
-{
-    bitset_index_t index = index + ULONG_BITS;
-    bitset_index_t velikost = bitset_size(jmeno_pole);
-    if (index >= velikost)
-    {
-        error_exit("bitset_getbit: Index %lu mimo rozsah 0..%lu", index - ULONG_BITS, velikost);
-    }
-    else
-    {
-        return (jmeno_pole[(index / ULONG_BITS) + 1] >> (index % ULONG_BITS)) & 1UL;
-    }
-}
+bitset_index_t bitset_getbit(bitset_t jmeno_pole, bitset_index_t index);
 
 #endif /* !USE_INLINE */
 
